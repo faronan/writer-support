d06b94613f1c68cac57b6dd90060cec0d1b8f083
faronan <toshiki.ito88@gmail.com>
2020-12-25 07:43:04 +0900

    🔧Add Next spec config

---
+++ packages/front/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^@/(.+)': '<rootDir>/src/$1',
+    '^@test/(.+)': '<rootDir>/test/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+};
+export default config;
