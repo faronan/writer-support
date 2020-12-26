1175d81645a1bd92cc37d11e471b228787c4009f
faronan <toshiki.ito88@gmail.com>
2020-12-26 23:14:42 +0900

    🔧Imports GraphQL modules

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ProofreadingDataResolver } from './resolvers/proofreadingData.resolver';
+
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, ProofreadingDataResolver],
 })
 export class AppModule {}
