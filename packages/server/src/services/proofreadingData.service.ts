c4d7f985d937d50e85117c75e5fc7355712914fb
faronan <toshiki.ito88@gmail.com>
2021-01-05 00:33:58 +0900

    🔧Add some params in createProofreading

--- packages/server/src/services/proofreadingData.service.ts
+++ packages/server/src/services/proofreadingData.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/services/prisma.service';
 import { LintRuleService } from '@/services/lintRule.service';
 import { LintResultService } from '@/services/lintResult.service';
+import { ProofreadingData } from '@/models/proofreadingData.model';
+import { LintRule } from '@/models/lintRule.model';
+import { LintResult } from '@/models/lintResult.model';
 
 @Injectable()
 export class ProofreadingDataService {
@@ -23,8 +26,43 @@ export class ProofreadingDataService {
   async create(text: string, ruleNames: string[]) {
     const rules = await this.ruleService.create(ruleNames);
     const result = await this.resultService.create(text, ruleNames);
-    return await this.prismaService.proofreadingData.create({
-      data: { text: text, rules: rules, result: result },
-    });
+    const prismaProofreadingData = await this.prismaService.proofreadingData.create(
+      {
+        data: { text: text, rules: rules, result: result },
+      },
+    );
+
+    // MEMO: prismaService.proofreadingData.createの戻り値にはruleとresultが含まれていない
+    // ruleとresultを入れたProofreadingDataを新たに作成してからクライアントに返す
+    const transformProofreadingData = this.transform(
+      prismaProofreadingData,
+      ruleNames,
+      result['create'],
+    );
+
+    return transformProofreadingData;
   }
+
+  private transform = (
+    prismaProofreadingData: { dataId: number; text: string },
+    ruleNames: string[],
+    result: LintResult[],
+  ) => {
+    const proofreadingData = new ProofreadingData();
+    proofreadingData.dataId = prismaProofreadingData.dataId;
+    proofreadingData.text = prismaProofreadingData.text;
+    proofreadingData.rules = ruleNames.map((n) => {
+      const rule = new LintRule();
+      rule.ruleName = n;
+      return rule;
+    });
+    proofreadingData.result = result.map((r) => {
+      const result = new LintResult();
+      result.message = r.message;
+      result.line = r.line;
+      result.column = r.column;
+      return result;
+    });
+    return proofreadingData;
+  };
 }
