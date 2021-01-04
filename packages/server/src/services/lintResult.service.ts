8e39aad9d6e53c333d62c0779e559148df6eee01
faronan <toshiki.ito88@gmail.com>
2021-01-04 14:54:26 +0900

    🔧Execute TextLint

--- packages/server/src/services/lintResult.service.ts
+++ packages/server/src/services/lintResult.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { TextLintEngine } from 'textlint';
 
 @Injectable()
 export class LintResultService {
   async create(text: string, ruleNames: string[]) {
+    const options = {
+      rules: ruleNames,
+      rulesConfig: ruleNames.reduce(
+        (obj, name) => ({ ...obj, [name]: true }),
+        {},
+      ),
+    };
+    const engine = new TextLintEngine(options);
+
+    const results = await engine.executeOnText(text);
+    const messages = results[0].messages;
     const dict = {
-      create: [{ message: 'hoge', line: 1, column: 1 }],
+      create: messages.reduce(
+        (obj, message) =>
+          obj.concat([
+            {
+              message: message.message,
+              line: message.line,
+              column: message.column,
+            },
+          ]),
+        [],
+      ),
     };
     return await dict;
   }
