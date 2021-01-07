faff18e3fdb28dac799cc1cdf37be2312a405242
faronan <toshiki.ito88@gmail.com>
2021-01-07 23:37:57 +0900

    🔧Fix models with schema

--- packages/server/src/dto/proofreadingData.dto.ts
+++ packages/server/src/dto/proofreadingData.dto.ts
@@ -4,6 +4,10 @@ import { InputType, Field } from '@nestjs/graphql';
 export class AddProofreadingDataInput {
   @Field((_type) => String)
   text: string;
+  @Field((_type) => String)
+  userEmail: string;
+  @Field((_type) => String)
+  userName: string;
   @Field((_type) => [String])
   ruleNames: string[];
 }
