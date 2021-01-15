b36728d4abd25222c94f90751f29dab50f54d77f
faronan <toshiki.ito88@gmail.com>
2021-01-15 21:31:46 +0900

    🔧Use include when prisma create

--- packages/server/src/services/proofreadingData.service.ts
+++ packages/server/src/services/proofreadingData.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/services/prisma.service';
 import { UserService } from '@/services/user.service';
 import { LintResultService } from '@/services/lintResult.service';
-import { ProofreadingData } from '@/models/proofreadingData.model';
-import { LintResult } from '@/models/lintResult.model';
-import { User } from '@/models/user.model';
 
 @Injectable()
 export class ProofreadingDataService {
@@ -34,45 +31,13 @@ export class ProofreadingDataService {
     const prismaProofreadingData = await this.prismaService.proofreadingData.create(
       {
         data: { text: text, user: user, result: result },
+        include: {
+          user: true,
+          result: true,
+        },
       },
     );
 
-    // MEMO: prismaService.proofreadingData.createの戻り値にはuserとresultが含まれていない
-    // userとresultを入れたProofreadingDataを新たに作成してからクライアントに返す
-    const transformProofreadingData = this.transform(
-      prismaProofreadingData,
-      userEmail,
-      userName,
-      result['create'],
-    );
-
-    return transformProofreadingData;
+    return prismaProofreadingData;
   }
-
-  private transform = (
-    prismaProofreadingData: { dataId: number; text: string; userId: number },
-    userEmail: string,
-    userName: string,
-    result: LintResult[],
-  ) => {
-    const proofreadingData = new ProofreadingData();
-    proofreadingData.dataId = prismaProofreadingData.dataId;
-    proofreadingData.text = prismaProofreadingData.text;
-
-    const user = new User();
-    user.userId = prismaProofreadingData.userId;
-    user.userEmail = userEmail;
-    user.userName = userName;
-    proofreadingData.user = user;
-
-    proofreadingData.result = result.map((r) => {
-      const result = new LintResult();
-      result.message = r.message;
-      result.ruleName = r.ruleName;
-      result.line = r.line;
-      result.column = r.column;
-      return result;
-    });
-    return proofreadingData;
-  };
 }
