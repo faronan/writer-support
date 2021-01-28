009a396ffe132761ca917417a0afdb9cf4ff7e1d
faronan <toshiki.ito88@gmail.com>
2021-01-28 22:59:59 +0900

    🚚Move page "/" => "/check"

--- packages/front/src/pages/index.tsx
+++ packages/front/src/pages/index.tsx
@@ -1,18 +1,5 @@
-import { useMutation } from '@apollo/client';
-import { CreateProofreadingDocument } from '@graphql/graphql-operations';
 import { Layout } from '@/components/templates/layout';
-import { AuthComponent } from '@/components/molecules/AuthComponent';
-import { ProofreadingComponent } from '@/components/organisms/ProofreadingComponent';
 
 export default function Home() {
-  const [, { loading: mutationLoading }] = useMutation(
-    CreateProofreadingDocument,
-  );
-
-  return (
-    <Layout isLoading={mutationLoading}>
-      <AuthComponent></AuthComponent>
-      <ProofreadingComponent></ProofreadingComponent>
-    </Layout>
-  );
+  return <Layout>{/* TODO: TOPにいい感じのページ作る */}</Layout>;
 }
