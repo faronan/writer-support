eee2d8510a8a648bf94f7ec37e0dc841a732e1d4
faronan <toshiki.ito88@gmail.com>
2021-02-09 08:57:57 +0900

    🔧Combine loading

--- packages/front/src/pages/check.tsx
+++ packages/front/src/pages/check.tsx
@@ -1,15 +1,36 @@
-import { useMutation } from '@apollo/client';
-import { CreateProofreadingDocument } from '@graphql/graphql-operations';
+import { useQuery, useMutation } from '@apollo/client';
+import {
+  CreateProofreadingDocument,
+  FindUserDocument,
+  CreateUserDocument,
+  CreateNgWordDocument,
+  CreateTemplateWordDocument,
+} from '@graphql/graphql-operations';
 import { Layout } from '@/components/templates/layout';
 import { ProofreadingComponent } from '@/components/organisms/ProofreadingComponent';
 
 export default function Check() {
-  const [, { loading: mutationLoading }] = useMutation(
+  const [, { loading: createProofreadingLoading }] = useMutation(
     CreateProofreadingDocument,
   );
+  const [, { loading: createUserLoading }] = useMutation(CreateUserDocument);
+  const [, { loading: createNgLoading }] = useMutation(CreateNgWordDocument);
+  const [, { loading: createTemplateLoading }] = useMutation(
+    CreateTemplateWordDocument,
+  );
+  const { loading: findUserLoading } = useQuery(FindUserDocument);
 
   return (
-    <Layout isLoading={mutationLoading} requiresAuth={true}>
+    <Layout
+      isLoading={
+        createProofreadingLoading ||
+        createUserLoading ||
+        createNgLoading ||
+        createTemplateLoading ||
+        findUserLoading
+      }
+      requiresAuth={true}
+    >
       <ProofreadingComponent></ProofreadingComponent>
     </Layout>
   );
